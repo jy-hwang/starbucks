6f065da95e94e49f6512566c6e213a57a3b18edf
jy-hwang <stratojy@gmail.com>
2023-08-23 13:09:35 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-//console.log('js');
-
-const searchEl = document.querySelector('.search');
-
-const searchInputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', () => {
-  //Logic
-  searchInputEl.focus();
-});
-
-searchInputEl.addEventListener('focus', () => {
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', 'Search...');
-});
-
-searchInputEl.addEventListener('blur', () => {
-  searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
-});
-
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -175,7 +154,3 @@ spyEls.forEach(function (spyEl) {
      .setClassToggle(spyEl, 'show')
      .addTo(new ScrollMagic.Controller());
 });
-
-const thisYear = document.querySelector('.this-year');
-
-thisYear.textContent = new Date().getFullYear();
